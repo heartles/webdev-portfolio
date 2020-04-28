74fd7fc5618f6e1ce82db700057cf4d7a1f4434b
heartles <dev@heartles.io>
2020-04-28 16:45:16 -0700

    Setup site templating

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 ## Libraries used:
 
 - Font Awesome
+- Golang standard library (for html templating)
 
 ## Tutorials used:
 
 - https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+
+## Building:
+
+The build process depends on Go. If you do not have it installed, grab it from your
+package manager or the [official site](https://golang.org).
+
+To build the static site files, execute:
+
+`go run main.go`
+
+This will build a static copy of the site to a subdirectory called `dist/`. To host
+this, use whatever static site hosting you wish.
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+const templateDir = "web/templates"
+const outputDir = "dist"
+const htmlBase = "web/base.html"
+
+func main() {
+	os.Mkdir(outputDir, 0755)
+
+	htmlBaseTemp := template.Must(template.New("").ParseFiles(htmlBase))
+
+	err := filepath.Walk(templateDir, func(path string, info os.FileInfo, _ error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		relPath, _ := filepath.Rel(templateDir, path)
+
+		outPath := filepath.Join(outputDir, relPath)
+
+		file, _ := os.Open(path)
+		defer file.Close()
+		outFile, err := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			panic(err)
+		}
+		defer outFile.Close()
+
+		switch filepath.Ext(info.Name()) {
+		case ".html":
+			return renderHTML(htmlBaseTemp, file, outFile)
+		default:
+			return render(file, outFile)
+		}
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	files, _ := ioutil.ReadDir(templateDir)
+	for _, file := range files {
+		if file.Name() == htmlBase {
+			continue
+		}
+	}
+}
+
+func renderHTML(base *template.Template, page *os.File, w io.Writer) error {
+	pageBytes, _ := ioutil.ReadAll(page)
+	return base.ExecuteTemplate(w, "base.html", template.HTML(pageBytes))
+}
+
+func render(page *os.File, w io.Writer) error {
+	_, err := io.Copy(w, page)
+	return err
+}

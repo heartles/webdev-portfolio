a86a050a0410534dc0bc8397998cd4e576b9396f
heartles <dev@heartles.io>
2020-05-01 19:54:23 -0700

    Add projects page

--- web/templates/index.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  console.log("toggling menu...");
-  var elem = document.getElementById("menu-items");
-  if (elem.classList.contains("open-menu")) {
-    elem.classList.remove("open-menu");
-  } else {
-    elem.classList.add("open-menu");
-  }
-}
